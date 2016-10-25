1bcf4d7c3f8e53b0e8f6bd85544bc34a5e571edf
Al Ilseman <al@ilseman.com>
2016-10-25 16:53:57 -0500

    silly little button-color changing function

--- my_app.js
+++ my_app.js
@@ -1,1 +1,6 @@
 // An empty javascript file with this one comment
+
+function changeColor(element) {
+    var newColor = document.getElementById('color-input').value;
+    element.style.backgroundColor = newColor;
+}
